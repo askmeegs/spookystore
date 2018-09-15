4a98b0af989e2b21cfbdb0a06c16af9333cdd97c
m-okeefe <megan037@gmail.com>
2018-09-15 09:47:11 -0700

    var issues

---
+++ grepcmds.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# Logs 
+kubectl get pods | grep "backend-" | cut -d ' ' -f 1 | xargs -I{} kubectl logs -f {}
+
+kubectl get pods | grep "frontend-" | cut -d ' ' -f 1 | xargs -I{} kubectl logs -f {}
+
+
+# Delete pod 
+kubectl get pods | grep "frontend-" | cut -d ' ' -f 1 | xargs -I{} kubectl delete pod {}
+
+
